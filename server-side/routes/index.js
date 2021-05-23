097c7360787223e09d483de6884e7b0726205d4f
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-05-23 20:10:50 +0900

    Mysql 연동

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 5,
+  user: 'developerTae',
+  password: '1234',
+  database: 'tutorial'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  pool.getConnection(function(err, connection){
+    // Use the connection
+    connection.query('SELECT * FROM board', function(err, rows){
+      if(err) console.err("err : " + err);
+      console.log("rows : " + JSON.stringify(rows));
+
+      res.render('index',  {title: 'test', rows: rows});
+      connection.release();
+
+    });
+  });
 });
 
 module.exports = router;
