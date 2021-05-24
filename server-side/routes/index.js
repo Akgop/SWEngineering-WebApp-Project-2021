720ba80a5373b40c3bb5021988023629639e44e8
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2021-05-24 11:54:44 +0900

    Mysql 공용 계정 테스트

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  user: 'developerTae',
-  password: '1234',
-  database: 'tutorial'
+  user: 'recipe_develop',
+  password: 'recipedev321',
+  database: 'recipedb'
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   connection.connect();
 
-  connection.query('SELECT * FROM board', function(err, results){
+  connection.query('SELECT * FROM company', function(err, results){
     if (err) console.error('err : ' + err);
     console.log(results);
     return res.json(results)
