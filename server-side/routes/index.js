575ebbfdf882d3de08bf86eccbb54bf4125ff37b
username <YuHyeonGuen.dev@gmail.com>
2021-05-24 18:24:43 +0900

    mysql sharing complete

--- server-side/routes/index.js
+++ server-side/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var connection = mysql.createConnection({
+  host: '10.20.17.168',
   user: 'recipe_develop',
   password: 'recipedev321',
   database: 'recipedb'
@@ -9,7 +10,7 @@ var connection = mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  connection.query('SELECT * FROM tbl_company', function(err, results){
+  connection.query('INSERT INTO tbl_company(company_email, company_pwd, company_name, company_tel, company_income) VALUES ("kw@kw.ac.kr", "kw123", "kwangwoon", "010-1234-5678", "10") ', function(err, results){
     if (err) console.error('err : ' + err);
     console.log(results);
     return res.json(results)
