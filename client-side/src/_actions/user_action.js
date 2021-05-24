275932e554d909a98a2cb2e547457698f1d0af8f
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-05-24 14:34:28 +0900

    temp

--- client-side/src/_actions/user_action.js
+++ client-side/src/_actions/user_action.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { response } from '../../../server-side/app';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataToSubmit){
     const request = axios.post('http://localhost:3030/login', dataToSubmit)
@@ -7,6 +8,16 @@ export function loginUser(dataToSubmit){
 
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+export function registerUser(dataToSubmit){
+    const request = axios.post('http://localhost:3030/register', dataToSubmit)
+        .then( response => response.data );
+    
+    return {
+        type: REGISTER_USER,
         payload: request
     }
 }
