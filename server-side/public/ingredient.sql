36273a7591cd5e31c64898f9b9b66572541142cb
David YoonChang Nam <skadbsckd0902@gmail.com>
2021-05-28 17:54:34 +0900

    added images

---
+++ server-side/public/ingredient.sql
@@ -0,0 +1,4 @@
+
+
+
+insert into tbl_ingredient(category_name, ingredient_image, ingredient_content) values (3, )
---
+++ server-side/public/product.sql
@@ -0,0 +1,182 @@
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "고추장2.jpg", 1000, 0, 5, "동원고추장", "고추장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "고추장3.jpg", 8000, 0, 4, "광운고추장", "고추장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "고춧가루2.jpg", 3000, 0, 5, "CJ고춧가루", "고춧가루");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "고춧가루3.jpg", 5000, 0, 5, "HJ고춧가루", "고춧가루");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "당근2.jpg", 1000, 0, 5, "동원당근", "당근");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "당근3.jpg", 1000, 0, 5, "CJ당근", "당근");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "대파2.jpg", 1000, 0, 5, "광운대파", "대파");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "대파3.jpg", 1000, 0, 5, "HJ대파", "대파");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "돼지고기 앞다리살2.jpg", 1000, 0, 5, "광운돼지고기 앞다리살", "돼지고기 앞다리살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "돼지고기 앞다리살3.jpg", 1000, 0, 5, "CJ돼지고기 앞다리살", "돼지고기 앞다리살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "마늘2.jpg", 1000, 0, 5, "동원마늘", "마늘");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "마늘3.jpg", 1000, 0, 5, "HJ마늘", "마늘");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "맛술2.jpg", 1000, 0, 5, "동원맛술", "맛술");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "맛술3.jpg", 1000, 0, 5, "광운맛술", "맛술");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "생강2.jpg", 1000, 0, 5, "CJ생강", "생강");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "생강3.jpg", 1000, 0, 5, "HJ생강", "생강");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "설탕2.jpg", 1000, 0, 5, "동원설탕", "설탕");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "설탕3.jpg", 1000, 0, 5, "광운설탕", "설탕");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "양파2.jpg", 1000, 0, 5, "CJ양파", "양파");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "양파3.jpg", 1000, 0, 5, "HJ양파", "양파");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "올리고당2.jpg", 1000, 0, 5, "동원올리고당", "올리고당");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "올리고당3.jpg", 1000, 0, 5, "광운올리고당", "올리고당");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "진간장2.jpg", 1000, 0, 5, "CJ진간장", "진간장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "진간장3.jpg", 1000, 0, 5, "HJ진간장", "진간장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "참기름2.jpg", 1000, 0, 5, "동원참기름", "참기름");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "참기름3.png", 1000, 0, 5, "광운참기름", "참기름");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "통깨2.jpg", 1000, 0, 5, "CJ통깨", "통깨");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "통깨3.jpg", 1000, 0, 5, "HJ통깨", "통깨");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "국간장2.jpg", 1000, 0, 5, "동원국간장", "국간장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "국간장3.jpg", 1000, 0, 5, "광운국간장", "국간장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "다진마늘2.jpg", 1000, 0, 5, "CJ다진마늘", "다진마늘");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "다진마늘3.jpg", 1000, 0, 5, "HJ다진마늘", "다진마늘");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "된장2.jpg", 1000, 0, 5, "동원된장", "된장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "된장3.jpg", 1000, 0, 5, "광운된장", "된장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "들기름2.jpg", 1000, 0, 5, "CJ들기름", "들기름");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "들기름3.jpg", 1000, 0, 5, "HJ들기름", "들기름");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "배추김치2.jpg", 1000, 0, 5, "동원배추김치", "배추김치");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "배추김치3.jpg", 1000, 0, 5, "배추김치", "배추김치");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "삼겹살2.jpg", 1000, 0, 5, "CJ삼겹살", "삼겹살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "삼겹살3.jpg", 1000, 0, 5, "HJ삼겹살", "삼겹살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "새우젓2.jpg", 1000, 0, 5, "CJ새우젓", "새우젓");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "새우젓3.jpg", 1000, 0, 5, "HJ새우젓", "새우젓");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "대패 삼겹살2.jpg", 1000, 0, 5, "동원대패삼겹살", "대패 삼겹살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "대패 삼겹살3.jpg", 1000, 0, 5, "광운대패삼겹살", "대패 삼겹살");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "두부2.jpg", 1000, 0, 5, "동원두부", "두부");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "두부3.jpg", 1000, 0, 5, "광운두부", "두부");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "쌈장2.jpg", 1000, 0, 5, "CJ쌈장", "쌈장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "쌈장3.jpg", 1000, 0, 5, "HJ쌈장", "쌈장");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "호박2.jpg", 1000, 0, 5, "CJ호박", "호박");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "호박3.jpg", 1000, 0, 5, "HJ호박", "호박");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "계란2.jpg", 1000, 0, 5, "동원계란", "계란");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "계란3.jpg", 1000, 0, 5, "광운계란", "계란");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "부침가루2.jpg", 1000, 0, 5, "동원부침가루", "부침가루");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "부침가루3.jpg", 1000, 0, 5, "광운부침가루", "부침가루");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (5, "새우2.jpg", 1000, 0, 5, "CJ새우", "새우");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (9, "새우3.jpg", 1000, 0, 5, "HJ새우", "새우");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "오징어2.jpg", 1000, 0, 5, "동원오징어", "오징어");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "오징어3.jpg", 1000, 0, 5, "광운오징어", "오징어");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (2, "청양고추2.jpg", 1000, 0, 5, "동원청양고추", "청양고추");
+
+insert into tbl_product(company_id, product_image, product_price, product_flag, product_avg_score, product_item_name, category_name)
+values (3, "청양고추3.jpg", 1000, 0, 5, "광운청양고추", "청양고추");
+
+
+
